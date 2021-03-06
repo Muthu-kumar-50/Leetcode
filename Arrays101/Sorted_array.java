60165e3371953cf464c02443c86b9751c5a22c4d
Muthu Kumar <muthu08612@gmail.com>
2021-03-06 09:45:50 +0530

    Update Sorted_array.java

--- Arrays101/Sorted_array.java
+++ Arrays101/Sorted_array.java
@@ -1,15 +1,16 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+       
         int p1 = 0;int p2=0;
+        
         int[] dup = new int[m+n];
         
         for(int i=0; i < m+n; i++){
 
             if (n==0){return;}
             
-            if (p1 < m && p2 <n && nums1[p1] < nums2[p2]){
-                dup[i] = nums1[p1];
-                
+            if (p1 < m && p2 <n && nums1[p1] < nums2[p2] || p1<m && p2>=n){
+                dup[i] = nums1[p1];    
                 p1++;
             }
             else{
@@ -17,14 +18,11 @@ class Solution {
                 p2++;
             }
         }
-        
         for(int j=0; j<dup.length; j++){
-            dup[j] = nums1[j];    
-        }
-        
+            nums1[j] = dup[j];    
+        }       
         for (int k = 0; k< dup.length; k++){
             System.out.println(dup[k]);
         }
-        
-       }
     }
+}
