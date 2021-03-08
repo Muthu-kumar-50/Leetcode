7e0ed94083b6939831d6eaac43ac79b02eb71d23
Muthu Kumar <muthu08612@gmail.com>
2021-03-08 10:14:10 +0530

    Update Check_if_N _and_double_exists.java

--- Arrays101/Check_if_N _and_double_exists.java
+++ Arrays101/Check_if_N _and_double_exists.java
@@ -12,3 +12,19 @@ class Solution {
         return false;
     }
 }
+
+// Time complexity O(n^2) and Space Complexity O(n)
+
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+      for(int i=0; i<arr.length; i++){
+          int target = arr[i];
+          for(int j=i+1; j<arr.length; j++){
+              if(target == 2*arr[j] ||(arr[j]%2==0  && target == arr[j]/2)){
+                  return true;
+              }
+          }
+      }
+    return false;
+    }
+}
